4dfda2b69a31d249549011f4c8b7955c83e6da1e
Matt Thomas <matt@codecaffeine.com>
2010-09-18 18:50:20 -0400

    Not needed anymore

---
+++ Animations/RQEnemySprite.h
@@ -0,0 +1,16 @@
+//
+//  RQEnemySprite.h
+//  RunQuest
+//
+//  Created by Matthew Thomas on 9/18/10.
+//  Copyright (c) 2010 Code/Caffeine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface RQEnemySprite : NSObject {
+
+}
+
+@end
---
+++ Animations/RQEnemySprite.m
@@ -0,0 +1,14 @@
+//
+//  RQEnemySprite.m
+//  RunQuest
+//
+//  Created by Matthew Thomas on 9/18/10.
+//  Copyright (c) 2010 Code/Caffeine. All rights reserved.
+//
+
+#import "RQEnemySprite.h"
+
+
+@implementation RQEnemySprite
+
+@end
