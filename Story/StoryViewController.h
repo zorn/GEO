76d1629f6d5babc8884f9b6ed3c11a3e369a19ff
Mike Zornek <mike@clickablebliss.com>
2010-09-29 11:37:30 -0400

    started adding story controller

---
+++ Story/StoryViewController.h
@@ -0,0 +1,16 @@
+//
+//  StoryViewController.h
+//  RunQuest
+//
+//  Created by Michael Zornek on 9/28/10.
+//  Copyright 2010 Clickable Bliss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface StoryViewController : UIViewController {
+
+}
+
+@end
