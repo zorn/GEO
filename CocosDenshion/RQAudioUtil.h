53d22861fbb3295ed8ebd944d7081c21883caba4
Matt Thomas <matt@codecaffeine.com>
2010-09-28 21:43:03 -0400

    RQAudioUtils

---
+++ CocosDenshion/RQAudioUtil.h
@@ -0,0 +1,16 @@
+//
+//  RQAudioUtil.h
+//  RunQuest
+//
+//  Created by Matthew Thomas on 9/28/10.
+//  Copyright 2010 Matthew Thomas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface RQAudioUtil : NSObject {
+
+}
+- (void)fadeBackgroundMusic:(CGFloat)time;
+@end
