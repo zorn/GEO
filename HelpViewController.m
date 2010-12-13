ac8270075efb5ea374cad3db9ef3c841cb086b37
Joe Walsh <joe.walsh@me.com>
2010-12-12 21:33:39 -0500

    Better Back/Forward support

--- HelpViewController.m
+++ HelpViewController.m
@@ -7,11 +7,11 @@
 //
 
 #import "HelpViewController.h"
-
+#import "RQConstants.h"
 
 @implementation HelpViewController
 
-@synthesize webView, backButton, forwardButton, toolbar;
+@synthesize webView, backButton, forwardButton, toolbar, activityIndicator;
 
 // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
 /*
@@ -32,16 +32,21 @@
 }
 
 
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (void)loadBase {
 	NSString *htmlPath = [_path stringByAppendingPathComponent:@"index.html"];
 	NSString *htmlString = [NSString stringWithContentsOfFile:htmlPath encoding:NSUTF8StringEncoding error:nil];
 	[webView loadHTMLString:htmlString baseURL:[NSURL fileURLWithPath:_path]];
 }
 
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	[self loadBase];
+}
+
 - (BOOL)webView:(UIWebView *)webView_ shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
 	if ( navigationType == UIWebViewNavigationTypeLinkClicked ) {
+		userDidClickLink = YES;
 		NSArray *items = [toolbar items];
 		if ( ![items containsObject:backButton] ) {
 			NSMutableArray *mutableItems = [items mutableCopy];
@@ -49,21 +54,41 @@
 			[mutableItems insertObject:forwardButton atIndex:1];
 			[toolbar setItems:mutableItems animated:YES];
 			[mutableItems release];
+			[backButton setEnabled:YES];
 		}
 	}
+	
 	return YES;
 }
 
+- (IBAction)goBack:(id)sender {
+	if ( webView.canGoBack ) {
+		[webView goBack];
+	}
+	else {
+		userDidClickLink = NO;
+		[self loadBase];
+	}
+}
+		
+- (IBAction)goForward:(id)sender {
+	if ( webView.canGoForward ) {
+		[webView goForward];
+	}
+}
+
 - (void)webViewDidStartLoad:(UIWebView *)webView_ {
-	
+	[activityIndicator startAnimating];
 }
 
 -(void)webViewDidFinishLoad:(UIWebView *)webView_ {
-	
+	forwardButton.enabled = [webView canGoForward];
+	backButton.enabled = ( [webView canGoBack] || userDidClickLink );
+	[activityIndicator stopAnimating];
 }
 
 - (void)webView:(UIWebView *)webView_ didFailLoadWithError:(NSError *)error {
-	
+	[activityIndicator stopAnimating];
 }
 
 /*
@@ -86,6 +111,10 @@
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 	self.webView = nil;
+	self.backButton = nil;
+	self.activityIndicator	 = nil;
+	self.toolbar = nil;
+	self.forwardButton = nil;
 }
 
 - (IBAction)doneButtonPressed:(id)sender {
@@ -96,6 +125,10 @@
 - (void)dealloc {
 	[_path release];
 	[webView release];
+	[backButton release];
+	[forwardButton release];
+	[toolbar release];
+	[activityIndicator release];
     [super dealloc];
 }
 
